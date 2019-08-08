7fca2adc5bfcddc043736aeacbcf7200ea97e166
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-08-08 00:23:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,7 @@ def find_element_index(array, value_to_find)
   array.length.times { |index|
     if array(index) == value_to_find
       return index
-  }
-end
-  return nil
+}
 end
 
 def find_max_value(array)
